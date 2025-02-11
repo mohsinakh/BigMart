09fd3fb62c41e7813192a7f21a91e22760205758
mohsinakh <142650406+mohsinakh@users.noreply.github.com>
2025-02-11 19:53:16 +0530

    Update HeroBanner.js

--- src/components/Home/HeroBanner.js
+++ src/components/Home/HeroBanner.js
@@ -4,7 +4,7 @@ import '../css/HeroBanner.css'; // Import the CSS file
 
 const HeroBanner = () => {
   return (
-    <div className="hero-banner" style={{ backgroundImage: "url('https://i.imgur.com/sM2dm29.jpg')" }}>
+    <div className="hero-banner" style={{ backgroundImage: "url('https://i.postimg.cc/cJM74tsV/s-M2dm29-Imgur.jpg')" }}>
       <div className="hero-content">
         <h1 className="display-4">Welcome to <span className="text-highlighted">BigMart</span></h1>
         <p className="lead">Discover amazing varieties of high-quality products that suit your style.</p>
